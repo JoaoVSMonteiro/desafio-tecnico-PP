8681e19e3a150ee3511ee109808200e0ce0a5928
JoaoVSMonteiro <joao_dj58@hotmail.com>
2025-08-08 16:43:11 -0300

    feat: Adiciona service

---
+++ desafio-tecnico/src/main/java/com/transferencia/desafio_tecnico/service/TransferenciaService.java
@@ -0,0 +1,60 @@
+package com.transferencia.desafio_tecnico.service;
+
+import com.transferencia.desafio_tecnico.model.dtos.TransferenciaResquestDto;
+import com.transferencia.desafio_tecnico.model.entity.Carteira;
+import com.transferencia.desafio_tecnico.model.entity.Usuario;
+import com.transferencia.desafio_tecnico.repository.CarteiraRepository;
+import com.transferencia.desafio_tecnico.repository.UsuarioRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class TransferenciaService {
+
+    private static final String AUTORIZADOR = "https://util.devi.tools/api/v2/authorize";
+    private static final String NOTIFICADOR = "https://util.devi.tools/api/v1/notify";
+
+    private final RestTemplate restTemplate = new RestTemplate();
+
+    private final UsuarioRepository usuarioRepository;
+    private final CarteiraRepository carteiraRepository;
+
+    public void transferencia(TransferenciaResquestDto request){
+        log.info("Iniciando Transferência");
+
+        Usuario pagador = usuarioRepository.findById(request.getPagador())
+                .orElseThrow(() -> new IllegalArgumentException("Pagador não encontrado."));
+
+        Usuario recebedor = usuarioRepository.findById(request.getRecebedor())
+                .orElseThrow(() -> new IllegalArgumentException("Recebedor não encontrado."));
+
+        if(pagador.isLojista()){
+            throw  new IllegalArgumentException("Lojista não pode realizar trasnferência.");
+        }
+
+        Carteira carteiraPagador = pagador.getCarteira();
+        Carteira carteiraRecebedor = recebedor.getCarteira();
+
+        if(carteiraPagador.getValor() < request.getValor()){
+            throw new IllegalArgumentException("Saldo insuficiente");
+        }
+
+        Boolean autorizador = restTemplate.getForObject(AUTORIZADOR, Boolean.class);
+        if (autorizador == null || !autorizador){
+            throw new IllegalStateException("Transferência não autorizada pelo serviço externo");
+        }
+
+        carteiraPagador.setValor(carteiraPagador.getValor() - request.getValor());
+        carteiraRecebedor.setValor(carteiraRecebedor.getValor() + request.getValor());
+
+        try{
+            restTemplate.postForLocation(NOTIFICADOR, pagador.getEmail());
+        }catch (Exception e){
+            log.error("Falha ao enviar notificação: " + e.getMessage());
+        }
+    }
+}
